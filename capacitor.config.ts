616dd166917378d219c663e173bec186fbd988a6
divyajyotiwcd3-bit <divyajyotiwcd3@gmail.com>
2026-08-09 16:18:28 +0530

    Switch to remote URL mode for Capacitor - fixes auth bypass bug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.vidhi.mitra',
   appName: 'Vidhi Mitra',
   webDir: 'out',
+  server: {
+    url: 'https://vidhi-mitra.shilpshakti.org.in',
+    cleartext: false
+  }
 };
 
 export default config;
