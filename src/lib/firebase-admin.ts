dcdde55c4341f3b7410eb41e4c3eb20ff0771119
divyajyotiwcd3-bit <divyajyotiwcd3@gmail.com>
2026-08-11 01:01:08 +0530

    Fix firebase-admin project ID detection on server

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  admin.initializeApp();
+  admin.initializeApp({
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
 }
 
 export const adminAuth = admin.auth();
